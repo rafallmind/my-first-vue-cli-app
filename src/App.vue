60294cb0df5543eefdee65a3fac8b95653484581
ANDRE Louis <louis.gabriel.andre@gmail.com>
2023-04-04 17:36:57 +0200

    AjoutBoutonGoogle

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <header>
     <div class="navbar is-fixed-top is-info" role="navigation" aria-label="main navigation">
@@ -16,7 +18,7 @@
 
       <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-start">
-          
+
           <router-link class="navbar-item" to="/">Page d'accueil</router-link>
 
           <router-link class="navbar-item" to="/PageCoach">A propos du coach</router-link>
@@ -39,41 +41,56 @@
 
         <div class="navbar-end">
           <div class="navbar-item">
+            <div id="pseudo" class="loginPlace" style="display = 'none'">
+              Disconnect
+            </div>
             <div class="buttons">
-              <a class="button is-light">
-                Se connecter
-              </a>
+              <GoogleLogin :callback="callback" id="boutonG" />
             </div>
           </div>
         </div>
       </div>
     </div>
   </header>
+
   <body class="mt-6 bg2">
     <div class="container">
-      <RouterView/>
+      <RouterView />
     </div>
   </body>
   <footer class="footer bg2">
-  <div class="content has-text-centered">
-    <p>
-      <strong>{{ nomApplication }}</strong> par 
-      <a href="https://www.linkedin.com/in/julian-aubert-4b6438174/">Julian Aubert</a> - 
-      <a href="https://www.linkedin.com/in/samuel-briand-44220/">Samuel Briand</a> - 
-      <a href="https://www.linkedin.com/in/louis-andre-298785171/">Louis André</a> - 
-      <a href="https://www.linkedin.com/in/maxime-perrin-12a5601a7/">Maxime Perrin</a>.
-    </p>
-  </div>
-</footer>
-  
+    <div class="content has-text-centered">
+      <p>
+        <strong>{{ nomApplication }}</strong> par
+        <a href="https://www.linkedin.com/in/julian-aubert-4b6438174/">Julian Aubert</a> -
+        <a href="https://www.linkedin.com/in/samuel-briand-44220/">Samuel Briand</a> -
+        <a href="https://www.linkedin.com/in/louis-andre-298785171/">Louis André</a> -
+        <a href="https://www.linkedin.com/in/maxime-perrin-12a5601a7/">Maxime Perrin</a>.
+      </p>
+    </div>
+  </footer>
 </template>
+<script setup>
+import { decodeCredential } from 'vue3-google-login'
+const callback = (response) => {
+  const userData = decodeCredential(response.credential)
+  console.log("Handle the userData", userData.name)
+  const valeur = document.querySelector('#pseudo');
+  const loginBouton = document.getElementById("boutonG");
+  valeur.textContent = "Bienvenue " + userData.name;
+  loginBouton.style.display = "none";
+  valeur.style.display = "block";
 
+  //loginBouton.style.display = "block";
+
+}
+</script>
 <script>
 
-export default{
+export default {
   name: 'AppView',
-  data(){
-    return{
+  data() {
+    return {
       nomApplication: ''
     }
   },
@@ -88,29 +105,43 @@ export default{
 </script>
 
 <style>
-  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
-  .c1{
-    color:#A15F00 !important;
-  }
-  .c2{
-    color:#ED9A24 !important;
-  }
-  .c3{
-    color:#40B3FF !important;
-  }
-  .c4{
-    color:#0C65A1 !important;
-  }
-  .bg1{
-    background-color: #A15F00 !important;
-  }
-  .bg2{
-    background-color: #ED9A24 !important;
-  }
-  .bg3{
-    background-color: #40B3FF !important;
-  }
-  .bg4{
-    background-color: #0C65A1 !important; 
-  }
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+
+.loginPlace {
+  color: #ffffff !important;
+  margin-right: 25px;
+  display: none;
+}
+
+.c1 {
+  color: #A15F00 !important;
+}
+
+.c2 {
+  color: #ED9A24 !important;
+}
+
+.c3 {
+  color: #40B3FF !important;
+}
+
+.c4 {
+  color: #0C65A1 !important;
+}
+
+.bg1 {
+  background-color: #A15F00 !important;
+}
+
+.bg2 {
+  background-color: #ED9A24 !important;
+}
+
+.bg3 {
+  background-color: #40B3FF !important;
+}
+
+.bg4 {
+  background-color: #0C65A1 !important;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vue3GoogleLogin from 'vue3-google-login'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(vue3GoogleLogin, {
+  clientId: '81922183478-jud8pfus2uov7rt64q6k5lhldojd25er.apps.googleusercontent.com'
+}).use(store).use(router).mount('#app')
