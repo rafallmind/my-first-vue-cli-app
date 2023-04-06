5d1d9efe3032a6cb86fcf1ea62a6d0fd0ae91090
Julian AUBERT <julian.aubert@etu.univ-nantes.fr>
2023-04-06 20:07:29 +0200

    Import Axios et GetData

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,11 @@
 
 import ActivityComponent from '@/components/FicheActivite.vue';
 import fetch from 'node-fetch';
+import axios from 'axios';
 
+var gsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSYY_mA0X_tnaoCir60aps-jnZIJEiRDh8OXuGoE7-BduonmY-BesOWjfHgtrIGhiLYuAccjYI9Jg9Z/pub?gid=0&single=true&output=csv";   
+var activityList = [];
+//var whoList = [];
 
 export default {
   name: 'HomeView',
@@ -22,12 +26,42 @@ export default {
       fiches: []
     }
   },
+  mounted(){
+    axios.get(gsheet_url).then(response=>(
+      parseData(response.data)
+    ))
+  },
   async created() {
     const response = await fetch('/activityDataConfig.json');
     const data = await response.json();
     this.fiches = data.fiches;
   }
 }
+
+
+function parseData(data) {   
+    var whoSet = new Set();
+    console.log(data);
+    //TODO : traitement de la data https://gist.github.com/edwardlorilla/b9bb22dcc06ba6fe98aeb4f95d4fdbb1#file-styles OU https://edwardize.blogspot.com/2018/04/parsinf-csv-file-import-javascript-vuejs.html 
+    /*
+    entries.forEach(function(value) {
+        var entry = {
+            "who": value.gsx$who.$t,
+            "title": value.gsx$title.$t,
+            "description": value.gsx$description.$t,
+            "status": value.gsx$status.$t,
+            "urlYtb": value.gsx$urlYtb.$t
+        };
+
+        // Add to the set of authors
+        whoSet.add(entry.who);
+
+        // Push entry into the list
+        activityList.push(entry);
+    });*/
+    whoList = Array.from(whoSet);
+    whoList.sort();
+  }
 </script>
 
 
